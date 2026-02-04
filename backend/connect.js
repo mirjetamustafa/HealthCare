124b6ae8035d7ccf1cf1de081a3af0ecca9a2523
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-02-04 14:24:24 +0100

    installing dependeces

---
+++ backend/connect.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
+
+const client = new MongoClient(process.env.ATLAS_URI)
+let database
+
+module.exports = {
+  connectToServer: async () => {
+    try {
+      await client.connect()
+      database = client.db('taskManager')
+      console.log('Connected to MongoDB')
+    } catch (err) {
+      console.error('Failed to connect to MongoDB:', err)
+      process.exit(1) // ndal serverin nëse nuk lidhet
+    }
+  },
+
+  getDb: () => {
+    if (!database) {
+      throw new Error('Database not initialized. Call connectToServer first.')
+    }
+    return database
+  },
+}
