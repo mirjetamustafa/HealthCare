e32e13b5bb1a4cae305c282dcbd0b105180c7f82
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2026-03-06 13:24:29 +0100

    Move data fetching logic to App.tsx for reuse across components

--- healthcareFrontend/src/App.tsx
+++ healthcareFrontend/src/App.tsx
@@ -15,7 +15,45 @@ import AdminRegister from './pages/AdminRegister'
 import { ToastContainer } from 'react-toastify'
 import { AuthProvider } from './lib/AuthContext'
 
+import { useEffect, useState } from 'react'
+import type { DoctorResponse } from './api/User/user.types'
+import { getDoctor } from './api/User/user.client'
+
 function App() {
+  const [doctors, setDoctors] = useState<DoctorResponse[]>([])
+  const [selectedDepartment, setSelectedDepartment] = useState<string>('all')
+
+  const fetchDoctors = async () => {
+    try {
+      const response = await getDoctor()
+      setDoctors(response.data)
+      console.log(response.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchDoctors()
+  }, [])
+
+  const formatText = (text: string) =>
+    text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
+
+  const departments = [
+    { label: 'All Departments', value: 'all' },
+    ...Array.from(new Set(doctors.map((doctor) => doctor.department))).map(
+      (department) => ({
+        label: formatText(department),
+        value: department,
+      }),
+    ),
+  ]
+
+  const filteredDoctors =
+    selectedDepartment === 'all'
+      ? doctors
+      : doctors.filter((doctor) => doctor.department === selectedDepartment)
   return (
     <div className="">
       <ToastContainer />
@@ -59,7 +97,11 @@ function App() {
               element={
                 <>
                   <Header role="public" />
-                  <Doctors />
+                  <Doctors
+                    filteredDoctors={filteredDoctors}
+                    departments={departments}
+                    setSelectedDepartment={setSelectedDepartment}
+                  />
                   <Footer />
                 </>
               }
--- healthcareFrontend/src/pages/Doctors.tsx
+++ healthcareFrontend/src/pages/Doctors.tsx
@@ -1,45 +1,18 @@
-import { useEffect, useState } from 'react'
 import DoctorsCard from '../components/doctors/DoctorsCard'
 import FilterBySpeciality from '../components/doctors/FilterBySpeciality'
 import OurMedicalTeam from '../components/doctors/OurMedicalTeam'
-import type { DoctorResponse } from '../api/User/user.types'
-import { getDoctor } from '../api/User/user.client'
 
-const Doctors = () => {
-  const [doctors, setDoctors] = useState<DoctorResponse[]>([])
-  const [selectedDepartment, setSelectedDepartment] = useState<string>('all')
+interface DoctorProps {
+  filteredDoctors: any[]
+  departments: string[]
+  setSelectedDepartment: (department: string) => void
+}
 
-  const fetchDoctors = async () => {
-    try {
-      const response = await getDoctor()
-      setDoctors(response.data)
-      console.log(response.data)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  useEffect(() => {
-    fetchDoctors()
-  }, [])
-
-  const formatText = (text: string) =>
-    text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
-
-  const departments = [
-    { label: 'All Departments', value: 'all' },
-    ...Array.from(new Set(doctors.map((doctor) => doctor.department))).map(
-      (department) => ({
-        label: formatText(department),
-        value: department,
-      }),
-    ),
-  ]
-
-  const filteredDoctors =
-    selectedDepartment === 'all'
-      ? doctors
-      : doctors.filter((doctor) => doctor.department === selectedDepartment)
+const Doctors = ({
+  filteredDoctors,
+  departments,
+  setSelectedDepartment,
+}: DoctorProps) => {
   return (
     <div className="">
       <OurMedicalTeam />
